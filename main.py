29512af83b15b833508cc4f41e117c0a00c0ec91
ludwAir <ludwikowski@gmail.com>
2024-01-13 11:54:36 +0100

    breaking change
    added prefixes to api call

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ import finance
 
 app = FastAPI()
 
+# https://fastapi.tiangolo.com/advanced/behind-a-proxy/
+# root_path = "/ludw"
+
+# bez przedrostka -> funkcje implementowane bezpośrednio w main.py
+# s -> strava.py
+#   /s/suffer
+#   /s/flash
+# f -> finance.py
+#   /f/currency
+
+
 @app.get("/health")
 async def get_server_health():
     return {"success": True, "message": "ludw strava server running"}
 
-@app.get("/flash")
+@app.get("/s/flash")
 async def get_flash():
     access_token = strava.get_access_token()
     df = strava.get_strava_df(access_token)
@@ -21,7 +32,7 @@ async def get_flash():
         "message": strava.get_flash(df)
     }
 
-@app.get("/suffer")
+@app.get("/s/suffer")
 async def get_suffer(btasks: BackgroundTasks):
     access_token = strava.get_access_token()
     df = strava.get_strava_df(access_token)
@@ -32,14 +43,14 @@ async def get_suffer(btasks: BackgroundTasks):
     return Response(buf.getvalue(), media_type='image/png')
 
 
-@app.get("/currency")
+@app.get("/f/currency")
 async def get_eur(btasks: BackgroundTasks):
     buf = io.BytesIO()
-    buf = finance.get_eur_chart()
+    buf = finance.get_cur_chart()
     
     btasks.add_task(buf.close)
     return Response(content=buf.getvalue(), media_type='image/png')
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
+    uvicorn.run("main:app", root_path="/ludw", host="0.0.0.0", port=5001, reload=True)
